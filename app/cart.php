f1ee6eff8bbd29773df2699c7fd3de958e9861f4
Annika Harmsen <annikat.harmsen1000@gmail.com>
2026-01-20 17:17:58 -0500

    fix: corrected adjust cart item quantity to 0 handling to delete item instead of respond with error

--- app/cart.php
+++ app/cart.php
@@ -41,11 +41,12 @@ switch ($method) {
         deleteItem($pdo, $user_id);
         break;
 
-    default: 
+    default:
         Response::error("Not a valid HTTP method for server endpoint.", 405);
 }
 
-function postAddItem($pdo, $user_id) {
+function postAddItem($pdo, $user_id)
+{
 
     // validate variables
     $photo_id  = $_POST['photo_id']   ?? null;
@@ -60,13 +61,13 @@ function postAddItem($pdo, $user_id) {
         // if item already exists, update quantity
         $existing_item = findExistingItem($pdo, $user_id, $photo_id, $format_id);
 
-        if ($existing_item) { 
+        if ($existing_item) {
 
             $new_qty = (int)$existing_item["quantity"] + (int)$quantity;
 
             updateQuantity($pdo, $existing_item["cart_item_id"], $new_qty); // sends response
         }
-        
+
         $sql = "
             INSERT INTO cart_items (user_id, photo_id, format_id, quantity)
             VALUES (:user_id, :photo_id, :format_id, :quantity)
@@ -89,7 +90,8 @@ function postAddItem($pdo, $user_id) {
 
 // HELPER FUNCTIONS
 
-function getItems($pdo, $user_id) {
+function getItems($pdo, $user_id)
+{
     try {
         $sql = "
             SELECT
@@ -108,7 +110,7 @@ function getItems($pdo, $user_id) {
             WHERE ci.user_id = :user_id
             ORDER BY ci.cart_item_id DESC
         ";
-        
+
         $items = DB::all($pdo, $sql, ['user_id' => $user_id]);
 
         // convert prices to float
@@ -139,28 +141,33 @@ function getItems($pdo, $user_id) {
     }
 }
 
-function patchQuantity($pdo, $user_id) {
+function patchQuantity($pdo, $user_id)
+{
 
     try {
 
         $data = json_decode(file_get_contents("php://input"), true);
 
-        // basic validation 
+        // basic validation
         $cart_item_id = $data["cart_item_id"] ?? null;
         $quantity = $data["quantity"] ?? null;
 
-        if (!$cart_item_id || !is_numeric($cart_item_id) || !is_numeric($quantity) || $quantity < 1) {
+        if (!$cart_item_id || !is_numeric($cart_item_id) || !is_numeric($quantity) || $quantity >= 0) {
             http_response_code(400);
             exit("Invalid or missing cart_item_id or quantity.");
         }
 
-        updateQuantity($pdo, $cart_item_id,$quantity);
+        if ($quantity === 0) {
+            deleteItem($pdo, $user_id);
+        }
+
+        updateQuantity($pdo, $cart_item_id, $quantity);
 
     } catch (PDOException $e) {
         http_response_code(500);
         exit("Internal server error: " . $e->getMessage());
-        
-    } catch (Exception $e){
+
+    } catch (Exception $e) {
         http_response_code(400);
         exit("Invalid data: " . $e->getMessage());
 
@@ -168,7 +175,8 @@ function patchQuantity($pdo, $user_id) {
 
 }
 
-function deleteItem($pdo, $user_id) {
+function deleteItem($pdo, $user_id)
+{
 
     try {
 
@@ -204,14 +212,15 @@ function deleteItem($pdo, $user_id) {
         http_response_code(500);
         exit("Internal server error: " . $e->getMessage());
 
-    } catch (Exception $e){
+    } catch (Exception $e) {
 
         http_response_code(400);
         exit("Invalid data: " . $e->getMessage());
 
     }
 }
-function findExistingItem($pdo, $user_id, $photo_id, $format_id) {
+function findExistingItem($pdo, $user_id, $photo_id, $format_id)
+{
     $sql = "
         SELECT cart_item_id, quantity
         FROM cart_items
@@ -228,11 +237,12 @@ function findExistingItem($pdo, $user_id, $photo_id, $format_id) {
     ];
 
     $cart_item = DB::one($pdo, $sql, $params);
-    
+
     return $cart_item;
 }
 
-function updateQuantity($pdo, $cartItemId, $newQuantity) {
+function updateQuantity($pdo, $cartItemId, $newQuantity)
+{
 
     try {
 
